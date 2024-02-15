493174aa15efc90f351a514d7662058c6b951ca6
Anna4931 <aniketpatel3172@gmail.com>
2024-02-15 09:22:50 +0530

    Day5

--- Day5/problems14.py
+++ Day5/problems14.py
@@ -3,12 +3,14 @@ A1 = [3, 2, 6, 4, 5]
 A2 = [1, 3, 2, 5]
 Min=A1[0]
 Max=A1[0]
+Sum=0
 for i in range(len(A1)):
     if A1[i]<Min:
         Min=A1[i]
     if A1[i]>Max:
         Max=A1[i]
-Sum = sum(A1)
+for num in A1:
+    Sum += num
 n = Max - Min + 1
 ssum = n * (n + 1) // 2
 if ssum == Sum:
